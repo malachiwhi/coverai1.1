8b0cfc202911a3226a046157e7c3dd44ade176d2
malachiwhi <malachiwhite31@gmail.com>
2023-03-14 13:50:42 -0400

    updated headline

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Welome to CoverAI </h1>
+        <textarea className = "Fname">Enter First name</textarea> 
+        <textarea className = "Lname">Enter Last name</textarea> 
+        <textarea className = "Apply for "> Company and position that you are applying for </textarea>
+        <textarea className = "positions" > Postion or related potions that you are applying for</textarea>
+        <textarea className = "years" > Number of years or experience</textarea>
+        <textarea className = "Skills and Projects" > Skills projects and certifications</textarea>
+        <textarea className = "why" > Why you want the postion</textarea>
       </header>
     </div>
   );
