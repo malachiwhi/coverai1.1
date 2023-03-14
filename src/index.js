bb037d0cb5f08e4260762139d2a7cf71de5b2cea
malachiwhi <malachiwhite31@gmail.com>
2023-03-14 15:01:58 -0400

    updated headline

--- src/index.js
+++ src/index.js
@@ -4,11 +4,22 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { ThemeProvider } from "@aws-amplify/ui-react";
+import { Amplify } from 'aws-amplify';
+import awsconfig from './aws-exports';
+import "@aws-amplify/ui-react/styles.css";
+import { studioTheme } from "./ui-components";
+Amplify.configure(awsconfig);
+import {
+  Component1 
+ } from './ui-components';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <ThemeProvider theme={studioTheme}>
     <App />
-  </React.StrictMode>
+    <Component1 />
+  </ThemeProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
