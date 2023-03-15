1b4f3fec25c6988f63188d7b7a767c3ee6738907
malachiwhi <malachiwhite31@gmail.com>
2023-03-15 11:20:12 -0400

    updated headline

--- src/index.js
+++ src/index.js
@@ -3,37 +3,23 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
 import { ThemeProvider } from "@aws-amplify/ui-react";
 import { Amplify } from 'aws-amplify';
-
 import awsconfig from './aws-exports';
-
 import "@aws-amplify/ui-react/styles.css";
 import { studioTheme } from "./ui-components";
-
-import {
-  Component17 
- } from './ui-components';
-
- import {
-  Component3 
- } from './ui-components';
-
- import {
-  Desktop1 
- } from './Desktop1'; 
+import {Desktop1 } from './ui-components'; 
+import { TextAreaField } from '@aws-amplify/ui-react';
+import { DataStore } from '@aws-amplify/datastore';
 
 
 Amplify.configure(awsconfig);
-
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <ThemeProvider theme={studioTheme}>
     <App />
-    <Desktop1 />
   </ThemeProvider>
 );
 
